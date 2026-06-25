3ddb31a919b0a0a2253261d2b84265b7fb020cb5
kaganguru <kaganguru@proton.me>
2026-06-25 08:32:15 -0400

    we have a logo now

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 /* eslint-env node */
+import Image from 'next/image'
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Head, Button } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
+import logo from './logo.png'
 import 'nextra-theme-docs/style.css'
 
 export const metadata = {
@@ -16,10 +18,12 @@ export default async function RootLayout({ children }) {
   const navbar = (
     <Navbar
       logo={
-        <div>
-          <b>Hyperreal</b>{' '}
-          <span style={{ opacity: '60%' }}>Documentation</span>
-        </div>
+        <Image
+          src={logo}
+          alt="Hyperreal"
+          height={24}
+          priority
+        />
       }
       children={<Button variant="default">Buy Now</Button>}
     />
@@ -28,7 +32,14 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head faviconGlyph="◈" backgroundColor={{ dark: 'rgb(10, 10, 10)' }} />
+      <Head faviconGlyph="◈" backgroundColor={{ dark: 'rgb(10, 10, 10)' }} color={{
+    hue: 37,
+    saturation: 0,
+    lightness: {
+      light: 20,
+      dark: 84
+    }
+  }}/>
       <body>
         <Layout
           navbar={navbar}
